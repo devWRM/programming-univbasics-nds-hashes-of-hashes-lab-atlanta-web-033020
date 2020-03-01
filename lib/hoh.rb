e106cfaeee29b8d46ef4455cb26c325a01fef8e2
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-03-01 16:05:59 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,12 +43,14 @@ def naming_system
   # HoH all the way to Species!
 
 
-  PORTION_1 = {
+  Kingdom = {
      label: "Kingdom",
      sub_category: {
        label: "Phylum",
        sub_category: {
          label: "Class"
+
+         
        }
      }
    }
