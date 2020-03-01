496fece9038dffefd2eb99bdb4db49bda09ceab8
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-03-01 15:47:22 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -1,5 +1,5 @@
 # Build a nested HoH of the classification of biological life on Earth based
-# on these commented-out snippets assigned to PORTION_1-PORTION_4. Return
+# on these commented-out snippets assigned to PORTION_1 through PORTION_4. Return
 # that HoH as the return value for the method "naming_system."
 #
 # The tests will guide your construction.
@@ -42,4 +42,5 @@ def naming_system
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
 
+      {}
 end
